d8d8adbc6112de36c282c933099c865194edf867
abdulkader33447 <abdulkader33447@gmail.com>
2025-03-02 23:33:35 +0600

    added title

--- scrpt/devboard.js
+++ scrpt/devboard.js
@@ -19,201 +19,184 @@ document.getElementById("child-2").addEventListener("click", function () {
 
 document.getElementById("date").valueAsDate = new Date();
 
-// let clickTime=[];
-function abc(){}
-function cardTitle1 (title){
-  alert("Board Update Successfully");
-  const value = getInnerTextById("nav-count");
-  const minesTask = getInnerTextById("mines");
+const button1 = document
+  .getElementById("card-1-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-  if (value > 0) {
-    const sum = value + 1;
-    setInnerText("nav-count", sum);
-    const mines = minesTask - 1;
-    setInnerText("mines", mines);
-  }
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
 
-  let newTime = new Date().toLocaleTimeString();
+    let newTime = new Date().toLocaleTimeString();
 
-  const activity = document.getElementById("activity-log");
-  const div = document.createElement("div");
-  div.classList.add("bg-[#F4F7FF]");
-  div.classList.add("text-centre");
-  div.classList.add("mb-4");
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
 
-  div.innerHTML = `
-  title ${newTime}
-  `;
-  activity.appendChild(div);
+    div.innerHTML = `
+    <h2>'You have Complete The Task Fix Mobile Button Issue at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
 
-  // this.disabled = true;
-};
+    this.disabled = true;
+  });
 
-// const button1 = document
-//   .getElementById("card-1-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+const button2 = document
+  .getElementById("card-2-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
+    let newTime = new Date().toLocaleTimeString();
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
 
-//     let newTime = new Date().toLocaleTimeString();
+    div.innerHTML = `
+    <h2>'You have Complete The Task Add Dark Mode at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
 
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    this.disabled = true;
+  });
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at ${newTime}'</h2>
-//     `;
-//     activity.appendChild(div);
+const button3 = document
+  .getElementById("card-3-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-//     this.disabled = true;
-//   });
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
 
-// const button2 = document
-//   .getElementById("card-2-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+    let newTime = new Date().toLocaleTimeString();
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at 12:48:15 PM'</h2>
-//     `;
-//     activity.appendChild(div);
+    div.innerHTML = `
+    <h2>'You have Complete The Task Optimize Home page  at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
 
-//     this.disabled = true;
-//   });
+    this.disabled = true;
+  });
 
-// const button3 = document
-//   .getElementById("card-3-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+const button4 = document
+  .getElementById("card-4-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at 12:48:15 PM'</h2>
-//     `;
-//     activity.appendChild(div);
+    let newTime = new Date().toLocaleTimeString();
 
-//     this.disabled = true;
-//   });
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
 
-// const button4 = document
-//   .getElementById("card-4-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+    div.innerHTML = `
+    <h2>'You have Complete The Task Add new emoji 🤲 at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    this.disabled = true;
+  });
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at 12:48:15 PM'</h2>
-//     `;
-//     activity.appendChild(div);
+const button5 = document
+  .getElementById("card-5-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-//     this.disabled = true;
-//   });
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
 
-// const button5 = document
-//   .getElementById("card-5-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+    let newTime = new Date().toLocaleTimeString();
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at 12:48:15 PM'</h2>
-//     `;
-//     activity.appendChild(div);
+    div.innerHTML = `
+    <h2>'You have Complete The Task Integrate OpenAI API at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
 
-//     this.disabled = true;
-//   });
+    this.disabled = true;
+  });
 
-// const button6 = document
-//   .getElementById("card-6-btn")
-//   .addEventListener("click", function () {
-//     alert("Board Update Successfully");
-//     alert("second alert");
-//     const value = getInnerTextById("nav-count");
-//     const minesTask = getInnerTextById("mines");
+const button6 = document
+  .getElementById("card-6-btn")
+  .addEventListener("click", function () {
+    alert("Board Update Successfully");
+    alert("second alert");
+    const value = getInnerTextById("nav-count");
+    const minesTask = getInnerTextById("mines");
 
-//     if (value > 0) {
-//       const sum = value + 1;
-//       setInnerText("nav-count", sum);
-//       const mines = minesTask - 1;
-//       setInnerText("mines", mines);
-//     }
-//     const activity = document.getElementById("activity-log");
-//     const div = document.createElement("div");
-//     div.classList.add("bg-[#F4F7FF]");
-//     div.classList.add("text-centre");
-//     div.classList.add("mb-4");
+    if (value > 0) {
+      const sum = value + 1;
+      setInnerText("nav-count", sum);
+      const mines = minesTask - 1;
+      setInnerText("mines", mines);
+    }
 
-//     div.innerHTML = `
-//     <h2>'You have Complete The Task Add Dark Mode at 12:48:15 PM'</h2>
-//     `;
-//     activity.appendChild(div);
+    let newTime = new Date().toLocaleTimeString();
 
-//     this.disabled = true;
-//   });
+    const activity = document.getElementById("activity-log");
+    const div = document.createElement("div");
+    div.classList.add("bg-[#F4F7FF]");
+    div.classList.add("text-centre");
+    div.classList.add("mb-4");
+
+    div.innerHTML = `
+    <h2>'You have Complete The Task Improve Job searching at ${newTime}'</h2>
+    `;
+    activity.appendChild(div);
+
+    this.disabled = true;
+  });
 
 document.getElementById("clear-history").addEventListener("click", function () {
   location.reload();
